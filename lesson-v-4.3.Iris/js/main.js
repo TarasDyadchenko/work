ab55bd953433a73de4d8e4b1eefae4308e605af5
TarasDyadchenko <taras.dyatchenko@gmail.com>
2019-06-29 20:35:12 +0300

    lesson-v-4.3.Iris

--- lesson-v-4.3.Iris/js/main.js
+++ lesson-v-4.3.Iris/js/main.js
@@ -1,5 +1,6 @@
 ;(function ($) {
     $(function () {
+        // tabs
         $('.blog span[data-id]').on('click', function () {
             var $this = $(this),
                 currentType = $this.data('id');
@@ -11,6 +12,19 @@
                 .addClass('active')
                 .siblings()
                 .removeClass('active');
+        });
+
+        // popup
+        $('.popup .close').on('click', function () {
+            $(this)
+                .closest('.popup_bg')
+                // .removeClass('opened')
+                .fadeOut();
+        });
+        $('.ask').on('click', function () {
+            $('.popup_bg')
+                .addClass('opened')
+                .fadeIn();
         });
     });
 })(jQuery);
