2f3eeb350787b53d42e816a0b3c1c745125667ca
TarasDyadchenko <taras.dyatchenko@gmail.com>
2019-11-10 19:32:38 +0200

    lesson-v-4.7

--- lesson-v-4.7/main.js
+++ lesson-v-4.7/main.js
@@ -1,7 +1,4 @@
-// ;(function($){
-//     $(function () {
-//         $('.hamburger, .close').on('click', function () {
-//             $('.menu, .hamburger').toggle();
-//         });
-//     });
-// })(jQuery);
+$("#tagcloud a").tagcloud({
+    size: {start: 12, end: 36, unit: "px"},
+    color: {start: '#3498DB', end: '#46CFB0'}
+});
